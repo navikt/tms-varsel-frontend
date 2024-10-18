736f80d76e33ba8d8a114ebc3910a571b3b31591
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-10-18 07:54:50 +0200

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
+const {TMS_APP_BASE_URL, TMS_MIN_SIDE_URL, TMS_VARSLER_API_URL} = loadEnv(import.meta.env, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
-  define:{
-    "import.meta.env.TMS_APP_BASE_URL":  process.env.TMS_APP_BASE_URL,
-    "import.meta.env.TMS_MIN_SIDE_URL":  process.env.TMS_MIN_SIDE_URL,
-    "import.meta.env.TMS_VARSLER_API_URL": process.env.TMS_VARSLER_API_URL,
+  define: {
+    "import.meta.env.TMS_APP_BASE_URL":process.env.TMS_APP_BASE_URL,
+    "import.meta.env.TMS_MIN_SIDE_URL":process.env.TMS_MIN_SIDE_URL,
+    "import.meta.env.TMS_VARSLER_API_URL":process.env.TMS_VARSLER_API_URL,
   },
   base: "/minside/varsler-ny",
   build: {
--- src/utils/server/urls.ts
+++ src/utils/server/urls.ts
@@ -2,4 +2,4 @@ export const loginUrl = `/minside/varsler-ny/oauth2/login?redirect=${import.meta
 export const baseUrl = import.meta.env.TMS_APP_BASE_URL;
 export const minSideUrl = import.meta.env.TMS_MIN_SIDE_URL;
 export const varselApiUrl = import.meta.env.TMS_VARSLER_API_URL;
-console.log(import.meta.env);
+console.log(import.meta.env)
