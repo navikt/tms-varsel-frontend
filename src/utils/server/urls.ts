3ca8e87959ea1aad9d5746ba5cb5e0119219e5b5
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-10-18 07:58:15 +0200

    debug

--- src/utils/server/urls.ts
+++ src/utils/server/urls.ts
@@ -1,5 +1,8 @@
+import * as process from "node:process";
+
 export const loginUrl = `/minside/varsler-ny/oauth2/login?redirect=${import.meta.env.TMS_APP_BASE_URL}`;
 export const baseUrl = import.meta.env.TMS_APP_BASE_URL;
 export const minSideUrl = import.meta.env.TMS_MIN_SIDE_URL;
 export const varselApiUrl = import.meta.env.TMS_VARSLER_API_URL;
-console.log(import.meta.env)
+console.log("import.meta",import.meta.env)
+console.log(process.env.TMS_APP_BASE_URL)
